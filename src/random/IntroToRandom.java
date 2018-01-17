fbb671e31eab90d9af3c3a05b8570a87f85822f0
tippur.sanchitha <tippur.sanchitha@SEMS-259-15.SEMS259.LAB>
2018-01-17 14:54:26 -0800

    XMAS PRESENTS AND THE RANDOM #

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -8,23 +8,23 @@ import java.util.Random;
 //1. Run the program.  What number appears in the console?
 //   Run it again. Is the number the same?
 
-
 public class IntroToRandom {
 	public static void main(String[] args) {
-		int r = 0;
-		
-		//2. Now make r random. 
-		//   Create an object of the Random class
-		//   Hint: new Random().nextInt()
+		int r = 99;
 
-		//3. Limit the random number between 0 and 100
+		// 2. Now make r random.
+		// Create an object of the Random class
+		// Hint: new Random().nextInt()
+		Random MATH = new Random();
+		// 3. Limit the random number between 0 and 100
+		 r = MATH.nextInt(101); 
+		// 4. Limit the random number between 25 and 75. Hint: (highest value - lowest
+		// value) + lowest value
+r= MATH.nextInt((75-25)+1)+25;
+		// 5. Challenge: Limit the random number between -222 and 88
 
-		//4. Limit the random number between 25 and 75. Hint: (highest value - lowest value) + lowest value
+		System.out.println(r);
+	
+		 }}
+	
 
-		//5. Challenge: Limit the random number between -222 and 88
-
-
-		
-		System.out.println(r); 
-	}
-}
